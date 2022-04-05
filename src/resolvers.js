49b2b2344c6a83410efabdb67f26a0dcdbebe319
EgorUshakovOfficial <egorushakov13@gmail.com>
2022-04-05 12:05:39 -0600

    first commit

---
+++ src/resolvers.js
@@ -0,0 +1,42 @@
+const resolvers = {
+    Query: {
+        // get all tracks, will be used to populate the homepage grid of our web client
+        tracksForHome: (_, __, { dataSources }) => {
+            return dataSources.trackAPI.getTracksForHome();
+        },
+        track: (_, { id }, { dataSources }) => {
+            return dataSources.trackAPI.getTrack(id);
+        }
+    },
+    Track: {
+        author: ({ authorId }, _, { dataSources }) => {
+            return dataSources.trackAPI.getAuthor(authorId);
+        },
+        modules: ({ id }, _, { dataSources }) => {
+            return dataSources.trackAPI.getTrackModules(id);
+        }
+    },
+
+    Mutation: {
+        incrementTrackViews: async (_, { id }, { dataSources }) => {
+            try {
+                const track = await dataSources.trackAPI.incrementTrackViews(id);
+                return {
+                    code: 200,
+                    success: true,
+                    message: `Successfully incremented number of views for track ${id}`,
+                    track
+                };
+            } catch (err) {
+                return {
+                    code: err.extensions.response.status,
+                    success: false,
+                    message: err.extensions.response.body,
+                    track: null
+                };
+            }
+        }
+    }
+};
+
+module.exports = resolvers; 
